a9f21d53a1fe2e911b3b4d53ad1997a923a3a6cf
Binh26 <114886963+Binh26@users.noreply.github.com>
2023-06-01 01:04:11 +0200

    add a validfile judge with a number greater than eight judgement,  also unnecessary comments removed

--- A2_Triangulation_Code/Triangulation/triangulation_method.cpp
+++ A2_Triangulation_Code/Triangulation/triangulation_method.cpp
@@ -29,7 +29,7 @@
 
 using namespace easy3d;
 Vector3D linear_triangulation(const Vector2D& x1, const Vector2D& x2, const Matrix34& P1, const Matrix34& P2);
-
+Vector3D non_linear_triangulation(const Vector2D& x1, const Vector2D& x2, const Matrix34& P1, const Matrix34& P2);
 /**
  * TODO: Finish this function for reconstructing 3D geometry from corresponding image points.
  * @return True on success, otherwise false. On success, the reconstructed 3D points must be written to 'points_3d'
@@ -45,101 +45,19 @@ bool Triangulation::triangulation(
         Vector3D &t    /// output: 3D vector, which is the recovered translation of the 2nd camera
 ) const
 {
-    /*
-    /// NOTE: there might be multiple workflows for reconstructing 3D geometry from corresponding image points.
-    ///       This assignment uses the commonly used one explained in our lecture.
-    ///       It is advised to define a function for the sub-tasks. This way you have a clean and well-structured
-    ///       implementation, which also makes testing and debugging easier. You can put your other functions above
-    ///       triangulation(), or put them in one or multiple separate files.
-
-    std::cout << "\nTODO: I am going to implement the triangulation() function in the following file:" << std::endl
-              << "\t    - triangulation_method.cpp\n\n";
-
-    std::cout << "[Liangliang]:\n"
-                 "\tFeel free to use any provided data structures and functions. For your convenience, the\n"
-                 "\tfollowing three files implement basic linear algebra data structures and operations:\n"
-                 "\t    - Triangulation/matrix.h  Matrices of arbitrary dimensions and related functions.\n"
-                 "\t    - Triangulation/vector.h  Vectors of arbitrary dimensions and related functions.\n"
-                 "\t    - Triangulation/matrix_algo.h  Determinant, inverse, SVD, linear least-squares...\n"
-                 "\tPlease refer to the above files for a complete list of useful functions and their usage.\n\n"
-                 "\tIf you choose to implement the non-linear method for triangulation (optional task). Please\n"
-                 "\trefer to 'Tutorial_NonlinearLeastSquares/main.cpp' for an example and some explanations.\n\n"
-                 "\tIn your final submission, please\n"
-                 "\t    - delete ALL unrelated test or debug code and avoid unnecessary output.\n"
-                 "\t    - include all the source code (and please do NOT modify the structure of the directories).\n"
-                 "\t    - do NOT include the 'build' directory (which contains the intermediate files in a build step).\n"
-                 "\t    - make sure your code compiles and can reproduce your results without ANY modification.\n\n" << std::flush;
-
-    /// Below are a few examples showing some useful data structures and APIs.
-
-    /// define a 2D vector/point
-    Vector2D b(1.1, 2.2);
-
-    /// define a 3D vector/point
-    Vector3D a(1.1, 2.2, 3.3);
-
-    /// get the Cartesian coordinates of a (a is treated as Homogeneous coordinates)
-    Vector2D p = a.cartesian();
-
-    /// get the Homogeneous coordinates of p
-    Vector3D q = p.homogeneous();
-
-    /// define a 3 by 3 matrix (and all elements initialized to 0.0)
-    Matrix33 A;
-
-    /// define and initialize a 3 by 3 matrix
-    Matrix33 T(1.1, 2.2, 3.3,
-               0, 2.2, 3.3,
-               0, 0, 1);
-
-    /// define and initialize a 3 by 4 matrix
-    Matrix34 M(1.1, 2.2, 3.3, 0,
-               0, 2.2, 3.3, 1,
-               0, 0, 1, 1);
-
-    /// set first row by a vector
-    M.set_row(0, Vector4D(1.1, 2.2, 3.3, 4.4));
-
-    /// set second column by a vector
-    M.set_column(1, Vector3D(5.5, 5.5, 5.5));
-
-    /// define a 15 by 9 matrix (and all elements initialized to 0.0)
-    Matrix W(15, 9, 0.0);
-    /// set the first row by a 9-dimensional vector
-    W.set_row(0, {0, 1, 2, 3, 4, 5, 6, 7, 8}); // {....} is equivalent to a std::vector<double>
-
-    /// get the number of rows.
-    int num_rows = W.rows();
-
-    /// get the number of columns.
-    int num_cols = W.cols();
-
-    /// get the the element at row 1 and column 2
-    double value = W(1, 2);
-
-    /// get the last column of a matrix
-    Vector last_column = W.get_column(W.cols() - 1);
-
-    /// define a 3 by 3 identity matrix
-    Matrix33 I = Matrix::identity(3, 3, 1.0);
-
-    /// matrix-vector product
-    Vector3D v = M * Vector4D(1, 2, 3, 4); // M is 3 by 4
-
-    ///For more functions of Matrix and Vector, please refer to 'matrix.h' and 'vector.h'
-    
-
-    // TODO: delete all above example code in your final submission
-    */
     //--------------------------------------------------------------------------------------------------------------
     // implementation starts ...
     // implement 8 points algorithm, first we conduct normalization
-    
+    // TODO: check if the input is valid
     if (points_0.size() != points_1.size())
     {
 		std::cout << "The number of points in two images are not equal!" << std::endl;
 		return false;
 	}   // examine whether the number of points in two images are equal
+    if (points_0.size() < 8) {
+        LOG(ERROR) << "no enough point pairs (" << points_0.size() << " loaded, but at least " << 8 << " needed)";
+        return false;
+    }
     Vector2D mean1(0, 0);
     Vector2D mean2(0, 0);
     for (int i = 0; i < points_0.size(); i++)
@@ -215,7 +133,8 @@ bool Triangulation::triangulation(
     std::cout<<"S line 204 is correct"<<std::endl;
     // get the last column of V
     Vector F = V.get_column(8);
-    // construct the fundamental matrix, recovered from normalized points
+
+    // estimate the fundamental matrix F; construct the fundamental matrix, recovered from normalized points
     Matrix33 F_normalized(F[0], F[1], F[2],
         				  F[3], F[4], F[5],
         				  F[6], F[7], F[8]);
@@ -236,6 +155,7 @@ bool Triangulation::triangulation(
     std::cout<<F_denormalized<<std::endl;
     std::cout<<"********************above is f noralized **********************"<<std::endl;
     // obtained the fundamental matrix, now we recover the relative pose
+
     //first we compute the essential matrix
     // compute the intrinsic matrix, using given focal length and principle point, and skewness assume to be 0
     Matrix33 K1(0.0);
@@ -247,8 +167,8 @@ bool Triangulation::triangulation(
     std::cout << K1 << std::endl;
     std::cout << K2 << std::endl;
     std::cout<<"********************above is K1, K2 **********************"<<std::endl;
-
-
+    
+    
     Matrix33 E = K2.transpose() * F_denormalized * K1;
     std::cout << E << std::endl;
     std::cout << "********************above is E **********************" << std::endl;
@@ -338,18 +258,22 @@ bool Triangulation::triangulation(
         Vector3D X2 = linear_triangulation(x0,x1,P,P2);
         Vector3D X3 = linear_triangulation(x0,x1,P,P3);
         Vector3D X4 = linear_triangulation(x0,x1,P,P4);
-
+        Vector3D X1_Q = R1*X1 + t1;
+        Vector3D X2_Q = R1*X2 + t2;
+        Vector3D X3_Q = R2*X3 + t1;
+        Vector3D X4_Q = R2*X4 + t2;
         // judge whether the points are in front of the camera
-        if (X1[2] > 0) {
+        if (X1[2] > 0 && X1_Q[2]>0) {
+
 			positive_count1++;
 		}
-        if (X2[2] > 0) {
+        if (X2[2] > 0 && X2_Q[2] > 0) {
             positive_count2++;
         }
-        if (X3[2] > 0) {
+        if (X3[2] > 0 && X3_Q[2] > 0) {
 			positive_count3++;
 		}
-        if (X4[2] > 0) {
+        if (X4[2] > 0 && X4_Q[2] > 0) {
             positive_count4++;
         }
     }
@@ -397,7 +321,7 @@ bool Triangulation::triangulation(
     std::cout<<"final R "<<R<<std::endl;
     std::cout<<"final t "<<t<<std::endl;
     // test wheter the r and t is correct
-    
+    // TODO: Reconstruct 3D points.
     // compute the 3D points, recover the 3D points from the 2D points
     for (int i = 0; i < points_0.size(); i++) {
         double pt0_x = points_0[i].x();
@@ -410,7 +334,7 @@ bool Triangulation::triangulation(
 
 
         // linear method to triangulate the 3D points AP = 0, where A is 4*3 matrix, P is 3*1 matrix, we can use SVD to solve this problem
-        Vector3D X1 = linear_triangulation(x0, x1, P, final_P);
+        Vector3D X1 = non_linear_triangulation(x0, x1, P, final_P); // linear_triangulation(x0,x1,P,P1);
         //std::cout<< X1 << std::endl;
 
         points_3d.push_back(X1);
@@ -436,33 +360,84 @@ bool Triangulation::triangulation(
     }
 
 
-
-
-
-
-    // TODO: check if the input is valid (always good because you never known how others will call your function).
-
-    // TODO: Estimate relative pose of two views. This can be subdivided into
-    //      - estimate the fundamental matrix F;
-    //      - compute the essential matrix E;
-    //      - recover rotation R and t.
-
-    // TODO: Reconstruct 3D points. The main task is
-    //      - triangulate a pair of image points (i.e., compute the 3D coordinates for each corresponding point pair)
-
-    // TODO: Don't forget to
-    //          - write your recovered 3D points into 'points_3d' (so the viewer can visualize the 3D points for you);
-    //          - write the recovered relative pose into R and t (the view will be updated as seen from the 2nd camera,
-    //            which can help you check if R and t are correct).
-    //       You must return either 'true' or 'false' to indicate whether the triangulation was successful (so the
-    //       viewer will be notified to visualize the 3D points and update the view).
-    //       There are a few cases you should return 'false' instead, for example:
-    //          - function not implemented yet;
-    //          - input not valid (e.g., not enough points, point numbers don't match);
-    //          - encountered failure in any step.
     return points_3d.size() > 0;
 }
 // linear method
+class MyObjective : public Objective_LM {
+public:
+    Vector2D x1;
+    Vector2D x2;
+    Matrix34 P1;
+    Matrix34 P2;
+    MyObjective(int num_func, int num_var, const Vector2D& X1, const Vector2D& X2, const Matrix34& p1, const Matrix34& p2) : Objective_LM(num_func, num_var) {
+        x1 = X1;
+        x2 = X2;
+        P1 = p1;
+        P2 = p2;
+        std::cout<<"x1 "<<x1<<std::endl;
+        std::cout<<"x2 "<<x2<<std::endl;
+        std::cout<<"P1 "<<P1<<std::endl;
+        std::cout<<"P2 "<<P2<<std::endl;
+        std::cout<<"num_func "<<num_func<<std::endl;
+        std::cout<<"num_var "<<num_var<<std::endl;
+
+    }
+
+    /**
+     *  Calculate the values of each function at x and return the function values as a vector in fvec.
+     *  @param  x           The current values of variables.
+     *  @param  fvec        Return the value vector of all the functions.
+     *  @return Return a negative value to terminate.
+     *
+     *  NOTE: This function implements f = (x0 - 1.0)^2 + (x1 - 1.0)^2. A client problem must implement
+     *      this function to evaluate the values of each function in the expression of x.
+     */
+    
+    int evaluate(const double* x, double* fvec) {
+
+        Vector4D X1_4D(x[0], x[1], x[2], 1);
+        Vector3D x1_hat = P1 * X1_4D;
+        double x1_hat_x = x1_hat[0] / x1_hat[2];
+        double x1_hat_y = x1_hat[1] / x1_hat[2];
+       
+        Vector3D x2_hat = P2 * X1_4D;
+        double x2_hat_x = x2_hat[0] / x2_hat[2]; 
+        double x2_hat_y = x2_hat[1] / x2_hat[2];
+//        std::cout<<"x1_hat_x "<<x1_hat_x<<std::endl;
+//        std::cout<<"x1_hat_y "<<x1_hat_y<<std::endl;
+//        std::cout<<"x2_hat_x "<<x2_hat_x<<std::endl;
+//        std::cout<<"x2_hat_y "<<x2_hat_y<<std::endl;
+//        std::cout<<"x1[0] "<<x1[0]<<std::endl;
+//        std::cout<<"x1[1] "<<x1[1]<<std::endl;
+//        std::cout<<"x2[0] "<<x2[0]<<std::endl;
+//        std::cout<<"x2[1] "<<x2[1]<<std::endl;
+
+        fvec[0] = x1_hat_x - x1[0];
+        fvec[1] = x1_hat_y - x1[1];
+        fvec[2] = x2_hat_x - x2[0];
+        fvec[3] = x2_hat_y - x2[1];
+
+        return 0;
+    }
+};
+Vector3D non_linear_triangulation(const Vector2D& x1, const Vector2D& x2, const Matrix34& P1, const Matrix34& P2) {
+    // call the tutorial optimizer
+    MyObjective obj(4, 3,x1,x2,P1,P2);
+
+    /// create an instance of the Levenberg-Marquardt (LM for short) optimizer
+    Optimizer_LM lm;
+
+    /// initialized the variables. Later x will be modified after optimization.
+    std::vector<double> x = { 4.0, -4.0,4.0 }; // Let's guess the initial values to be (4.0, 4.0)
+
+    /// optimize (i.e., minimizing the objective function).
+    bool status = lm.optimize(&obj, x);
+    
+    /// retrieve the result.
+    std::cout << "the solution is:     " << x[0] << ", " << x[1] <<" " << x[2] << std::endl;
+    return Vector3D(x[0], x[1], x[2]);
+
+}
 Vector3D linear_triangulation(const Vector2D& x1, const Vector2D& x2, const Matrix34& P1, const Matrix34& P2) {
 	// construct the matrix A
 
@@ -507,10 +482,6 @@ Vector3D linear_triangulation(const Vector2D& x1, const Vector2D& x2, const Matr
     X[0] = X_4D[0]/ X_4D[3];
     X[1] = X_4D[1]/ X_4D[3];
     X[2] = X_4D[2]/ X_4D[3];
-
-    
-   
-   
 
 
 	return X;
